486f0d277f80b5be11e6a0c8daa31a5aa4cdbbda
PabloxDxD <99362268+PabloxDxD@users.noreply.github.com>
2022-07-26 16:20:37 -0600

    Commit

--- ejemplo11.cpp
+++ ejemplo11.cpp
@@ -7,9 +7,10 @@ int main(void){
     cout << "Impresion de numeros pares \n";
     for(int i=0; i<=100; i=i+2){
         if(i==50){
-            cout << "No se imprimo el numero ";
+            cout << "No se imprimo el numero 50" << endl;
+        }else{
+            cout << i << endl;
         }
-        cout << i << endl;
     }
 
     //Imprimir los numeros impares entre 1 y 100
@@ -17,9 +18,10 @@ int main(void){
     cout << "Impresion de numeros impares \n";
     for(int i = 1; i<=100; i=i+2){
         if(i == 57){
-            cout << "No se imprimo el numero ";
+            cout << "No se imprimo el numero 57" << endl;
+        }else{
+            cout << i << endl;
         }
-        cout << i << endl;
     }
 
 }
